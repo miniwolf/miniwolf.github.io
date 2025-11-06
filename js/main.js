1711f8a35bbda1645695d2b879e27180fb1c827c
Nicklas Pingel <nicklas@unity3d.com>
2025-11-06 21:48:26 +0100

    Fix issue with mobile closing on mobile

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 
-// Close mobile menu when clicking on a link
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-}));
+// // Close mobile menu when clicking on a link
+// document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+//     hamburger.classList.remove('active');
+//     navMenu.classList.remove('active');
+// }));
 
 // Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
